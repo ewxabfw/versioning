5ba37cecc24e5ecf39fc4c8dee680a39fe8c1907
Oleg Sobolev <iiiyxep@list.ru>
2023-08-10 18:02:00 +0200

    chore(MCKORD-1): added jira ticket check

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,18 @@
-module.exports = { extends: ["@commitlint/config-conventional"] };
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    'jira-ticket-in-scope': [2, 'always'],
+  },
+  plugins: [
+    {
+      rules: {
+        'jira-ticket-in-scope': ({scope}) => {
+          return [
+            scope?.match(/MCKORD-[0-9]+/g),
+            `Scope must contain a JIRA ticket reference. Example: "feat(MCKORD-123): description"`,
+          ];
+        },
+      },
+    },
+  ],
+};
